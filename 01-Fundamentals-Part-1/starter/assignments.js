02763eefb0e41606b444029e14777ac8019cf273
zdarova90210 <zdarova90210@gmail.com>
2020-11-16 12:21:40 +0300

    Values and Variables [done]

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = 'Russia';
+let continent = 'Eurasia';
+let population = 144.5;
+
+console.log(`Country: ${country}`);
+console.log(`Continent: : ${continent}`);
+console.log(`Population: ${population}`);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,10 @@
 // Linking a JavaScript File
 let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let firstName = 'Matilda';
+
+// console.log(40 + 8 + 23 - 10);
+// console.log('Jonas');
+// console.log(23);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
