958ff795d9f3044f810668f843e30218632d2ea3
Maksim Mirbabaev <mmirbabaev@yotateam.com>
2020-12-02 23:50:50 +0300

    basic operators + challenge #1 [done]

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,24 +1,84 @@
 ////////////////////////////////////
 // Linking a JavaScript File
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
 ////////////////////////////////////
 // Values and Variables
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
 ////////////////////////////////////
 // let, const and var
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-const birthYear = 1991;
+// const birthYear = 1991;
 // birthYear = 1990;
 // const job;
+// var job = 'programmer';
+// job = 'teacher'
+// lastName = 'Schmedtmann';
+// console.log(lastName);
 
-var job = 'programmer';
-job = 'teacher'
+////////////////////////////////////
+// Basic operators
 
-lastName = 'Schmedtmann';
-console.log(lastName);
+// Math operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+//  2 ** 3 means 2 to the power of 3 = 2 * 2 * 2;
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1 = 101
+// x-- // x = x - 1 = 100
+// x-- // x = x - 1 = 99
+// console.log(x);
+
+// Comparison operators
+// console.log(ageJonas > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+// console.log(now - 1991 > now - 2018);
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+//
+// console.log(now - 1991 > now - 2018);
+//
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10, x = 10
+// console.log(x, y);
+//
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah, averageAge);
+
+////////////////////////////////////
+// Coding Challenge #1
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(`Mark's BMI: ${BMIMark}, John's BMI: ${BMIJohn}, is Mark's BMI higher: ${markHigherBMI}`);
