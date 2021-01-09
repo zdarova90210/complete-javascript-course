8a68d00bb4b008d0baab7c52702cd3a302d28422
zdarova90210 <zdarova90210@gmail.com>
2021-01-10 01:36:08 +0300

    ype coercion [done]

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -132,17 +132,41 @@
 
 ////////////////////////////////////
 // Coding Challenge #2
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 78;
-const heightJohn = 1.69;
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 78;
+// const heightJohn = 1.69;
+//
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// if (BMIMark > BMIJohn) {
+//     console.log(`Mark's BMI (${BMIMark}) is higher than Jhon's (${BMIJohn})!`);
+// } else if (BMIMark < BMIJohn) {
+//     console.log(`Mark's BMI (${BMIMark}) is lower than Jhon's (${BMIJohn})!`);
+// } else {
+//     console.log(`Mark's BMI (${BMIMark}) is equal Jhon's (${BMIJohn})!`);
+// }
 
-const BMIMark = massMark / heightMark ** 2;
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-if (BMIMark > BMIJohn) {
-    console.log(`Mark's BMI (${BMIMark}) is higher than Jhon's (${BMIJohn})!`);
-} else if (BMIMark < BMIJohn) {
-    console.log(`Mark's BMI (${BMIMark}) is lower than Jhon's (${BMIJohn})!`);
-} else {
-    console.log(`Mark's BMI (${BMIMark}) is equal Jhon's (${BMIJohn})!`);
-}
+
+////////////////////////////////////
+// Type Conversion and Coercion
+
+// type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+console.log('23' > '18');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);
