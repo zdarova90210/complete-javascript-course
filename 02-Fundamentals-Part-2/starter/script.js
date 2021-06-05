3cfa3808cbcb269e7dea79bb3edc976d080af944
Maksim Mirbabaev <mmirbabaev@yotateam.com>
2021-06-05 19:38:49 +0300

    Arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -55,3 +55,19 @@ const num = Number('23');
 // const age2 = calcAge2(1991);
 //
 // console.log(age1, age2);
+
+///////////////////////////////////////
+// Arrow functions
+
+// const calcAge3 = birthYeah => 2037 - birthYeah;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+//
+// const yearsUntilRetirement = (birthYeah, firstName) => {
+//     const age = 2037 - birthYeah;
+//     const retirement = 65 - age;
+//     // return retirement;
+//     return `${firstName} retires in ${retirement} years`;
+// }
+//
+// console.log(yearsUntilRetirement(1991, 'Jonas')); console.log(yearsUntilRetirement(1980, 'Bob'));
